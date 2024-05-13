57ed517ea5edb0eef1f92530e27b82e4c122f2da
Daniel Garcia <ateliermx@outlook.com>
2024-05-12 23:53:57 -0600

    Update UselessCodes.py

--- UselessCodes.py
+++ UselessCodes.py
@@ -10,3 +10,25 @@ print(True < False)
 print("\"I'm\"\n\"\"learning\"\"\n\"\"\"Python\"\"\"")
 print("En aplicaciones de resta, el operador de resta espera dos argumentos: el izquierdo un minuendo en términos aritméticos y el derecho (un sustraendo).")
 print (2 + 3 * 5)
+##
+john = 3
+mary = 5
+adam = 6
+
+print(john, mary, adam, sep=',')
+
+total_apples = john + mary + adam
+print("total=", total_apples)
+##
+
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers / 1.61
+
+print(miles, "millas son", round(miles_to_kilometers, 4), "kilómetros")
+print(kilometers, "kilómetros son", round(kilometers_to_miles, 2), "millas")
+
+##
+
